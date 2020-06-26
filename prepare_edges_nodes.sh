70b0cada2f344c859233bc8e95871ea37ec23c6c
realmarcin <4625870+realmarcin@users.noreply.github.com>
2020-06-25 20:07:14 -0700

    quick script to make nodes and edges

---
+++ prepare_edges_nodes.sh
@@ -0,0 +1,12 @@
+echo "subject	object" > header.tsv
+cut -f1,2 masterG.edgelist.tsv > masterG_edges_raw.tsv
+cat header.tsv masterG_edges_raw.tsv > masterG_edges.tsv
+sed -i '' 's/: /:/g' masterG_edges.tsv
+sed -i '' 's/ /\~/g' masterG_edges.tsv
+cut -f1 masterG_edges.tsv > masterG_edges_col1.tsv
+cut -f2 masterG_edges.tsv > masterG_edges_col2.tsv
+cat masterG_edges_col1.tsv masterG_edges_col2.tsv > masterG_nodes_all.tsv
+uniq masterG_nodes_all.tsv | sort > masterG_nodes.tsv
+mkdir me_data
+cp masterG_nodes.tsv masterG_edges.tsv masterG.edgelist.tsv me_data/
+tar -zcf me_data.tgz me_data
