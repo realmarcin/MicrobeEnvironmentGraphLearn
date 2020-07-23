eadd6468130ca2441d0055f16bf74f10e7fbf874
realmarcin <4625870+realmarcin@users.noreply.github.com>
2020-07-22 18:09:35 -0700

    added steps for creating SNAP node2vec input file

--- prepare_edges_nodes.sh
+++ prepare_edges_nodes.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo "subject	object" > header.tsv
 cut -f1,2 masterG.edgelist.tsv > masterG_edges_raw.tsv
 cat header.tsv masterG_edges_raw.tsv > masterG_edges.tsv
@@ -7,6 +9,13 @@ cut -f1 masterG_edges.tsv > masterG_edges_col1.tsv
 cut -f2 masterG_edges.tsv > masterG_edges_col2.tsv
 cat masterG_edges_col1.tsv masterG_edges_col2.tsv > masterG_nodes_all.tsv
 uniq masterG_nodes_all.tsv | sort > masterG_nodes.tsv
-mkdir me_data
-cp masterG_nodes.tsv masterG_edges.tsv masterG.edgelist.tsv me_data/
-tar -zcf me_data.tgz me_data
+
+curl https://genomics.lbl.gov/~marcin/MAK/MAK.jar > MAK.jar
+tail -n+2 masterG_edges.tsv > masterG_edges_nohead.tsv
+java -classpath MAK.jar DataMining.util.EdgestoInts masterG_edges_nohead.tsv
+
+#mkdir -p me_data
+#cp masterG_nodes.tsv masterG_edges.tsv masterG.edgelist.tsv me_data/
+#tar -zcf me_data.tgz me_data
+
+
